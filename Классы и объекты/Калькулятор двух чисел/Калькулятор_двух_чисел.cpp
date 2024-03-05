b25cdf3d29b32d99a416896a1266d79a6baca521
Arina Shvedova <arina4shvedova@gmail.com>
2024-03-05 17:36:55 +0300

    the Second lesson

---
+++ Классы и объекты/Калькулятор двух чисел/Калькулятор_двух_чисел.cpp
@@ -0,0 +1,106 @@
+﻿
+#include <iostream>
+
+class Calculator
+{
+public:
+    double num1 = 0;
+    double num2 = 0;
+
+    Calculator(double num1, double num2)
+    {
+        this->num1 = num1;
+        this->num2 = num2;
+    };
+
+    double add()
+    {
+        return num1 + num2;
+    };
+    double multiply()
+    {
+        return num1 * num2;
+    };
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    };
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    };
+    double divide_1_2()
+    {
+        return num1 / num2;
+    };
+    double divide_2_1()
+    {
+        return num2 / num1;
+    };
+    bool set_num1(double num1)
+    {
+        if (num1 != 0) {
+            this->num1 = num1;
+            if (this->num1 == num1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            std::cout << "Неверный ввод!" << "\n";
+            return false;
+        }
+    };
+    bool set_num2(double num2)
+    {
+        if (num2 != 0) {
+            this->num2 = num2;
+            if (this->num2 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else {
+            std::cout << "Неверный ввод!" << "\n";
+            return false;
+        }
+    };
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    do {
+        double num1, num2;
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+
+        Calculator calculator(num1, num2);
+       
+        if (calculator.set_num1(num1) == 1 && calculator.set_num2(num2) == 1)
+        {
+            std::cout << "num1 + num2 = " << calculator.add() << "\n";
+            std::cout << "num1 - num2 = " << calculator.subtract_1_2() << "\n";
+            std::cout << "num2 - num1 = " << calculator.subtract_2_1() << "\n";
+            std::cout << "num1 * num2 = " << calculator.multiply() << "\n";
+            std::cout << "num1 / num2 = " << calculator.divide_1_2() << "\n";
+            std::cout << "num2 / num1 = " << calculator.divide_2_1() << "\n";
+
+        }
+        else
+            continue;
+    } while (true);
+    
+    return 0;
+}
---
+++ Классы и объекты/Счетчик/Счетчик.cpp
@@ -0,0 +1,115 @@
+﻿
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+class Counter
+{
+private:
+    int num;
+public: 
+    int increaseCounter()
+    {
+        return num++;
+    };
+    int reduceCounter()
+    {
+        return num--;
+    };
+    void showCounter()
+    {
+        std::cout << num << "\n";
+    };
+    Counter()
+    {
+        num = 1;
+    };
+    Counter(int num)
+    {
+        this->num = num;
+    }
+};
+
+void coosingCommand(Counter& counter, char users_response)
+{
+    switch (users_response)
+    {
+    case '+':
+    {
+        counter.increaseCounter();
+        break;
+    }
+    case '-':
+    {
+        counter.reduceCounter();
+        break;
+    }
+    case '=':
+    {
+        counter.showCounter();
+        break;
+    }
+    case 'х':
+    {
+        std::cout << "До свидания! ";
+        break;
+
+    }
+    default:
+    {
+        std::cout << "Ошибка " << "\n";
+        break;
+
+    }
+    }
+}
+
+int main()
+{ 
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    //setlocale(LC_ALL, "RUS");
+
+    char users_response;
+    std::string answer;
+    int num = 0;
+
+    std::cout << "Вы хотите указать начальное значение счетчика? Введите да или нет: ";
+    getline(std::cin, answer);
+    
+    if (answer == "да" || answer == "Да" || answer == "ДА")
+    {
+        std::cout << "Введите начальное значение счетчика: ";
+        std::cin >> num;
+
+        Counter counter(num);
+
+        do {
+            std::cout << "Введите команду('+', '-', '=' или 'x'): ";
+            std::cin >> users_response;
+            coosingCommand(counter, users_response);
+            if (users_response == 'х' || users_response == 'Х')
+            {
+                break;
+            }
+        } while (true);
+    }
+    else
+    {
+        Counter counter;
+        do {
+            std::cout << "Введите команду('+', '-', '=' или 'x'): ";
+            std::cin >> users_response;
+            coosingCommand(counter, users_response);
+            if (users_response == 'х' || users_response == 'Х')
+            {
+                break;
+            }
+        } while (true);
+
+    }
+
+  return 0;
+        
+   }
