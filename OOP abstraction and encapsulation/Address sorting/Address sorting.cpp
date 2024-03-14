1a9220d2d4867fc44f18ac6c92d1bc8a44599b25
Arina Shvedova <arina4shvedova@gmail.com>
2024-03-14 20:14:05 +0300

    lesson №3

---
+++ OOP abstraction and encapsulation/Address sorting/Address sorting.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+
+class Address {
+private:
+	std::string city;
+	std::string street;
+	int houseNum;
+	int flatNum;
+public:
+	Address(std::string city, std::string street, int houseNum, int flatNum) {
+		this->city = city;
+		this->street = street;
+		this->houseNum = houseNum;
+		this->flatNum = flatNum;
+	};
+	std::string getOutputAddress(){
+		return city + ", " + street + ", " + std::to_string(houseNum) + ", " + std::to_string(flatNum);
+	}
+};
+
+void sort(std::vector<std::string>& arr, int num)
+{
+	for (int i = 0; i < num; i++) {
+		for (int j = i+1; j < num ; j++) {
+			std::string str1 = arr[i];
+			std::string str2 = arr[j];
+			if (str1[0] >= str2[0]) {
+				std::string str3 = arr[i];
+				arr[i] = arr[j];
+				arr[j] = str3;
+			}
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int num = 0;
+	std::vector<std::string> arr;
+	std::ifstream file("in.txt");
+	if (file.is_open()) {
+		file >> num;
+		for (int i = 0; i < num; i++) {
+			std::string city, street;
+			int houseNum, flatNum;
+			file >> city;
+			file >> street;
+			file >> houseNum;
+			file >> flatNum;
+			Address address(city, street, houseNum, flatNum);
+			arr.push_back(address.getOutputAddress());
+		}
+	}
+	file.close();
+	
+	sort(arr, num);
+	std::ofstream out("out.txt");
+	if (out.is_open()) {
+		out << num << "\n";
+		for (int i = 0; i < num; i++) {
+			out << arr[i] << "\n";
+		}
+	}
+	out.close();
+	
+	return 0;
+}
---
+++ OOP abstraction and encapsulation/Address_1/Address.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Address {
+private:
+	std::string num;
+	std::vector<std::string> city;
+	std::vector<std::string> street;
+	std::vector<std::string> houseNum;
+	std::vector<std::string> flatNum;
+public:
+	Address(std::string fileName){ 
+		std::ifstream file(fileName);
+		if (file.is_open()) {
+			file >> num;
+			for (int i = 0; i < std::stoi(num); i++) {
+				std::string a, a1, a2, a3;
+				file >> a;
+				file >> a1;
+				file >> a2;
+				file >> a3;;
+				city.push_back(a);
+				street.push_back(a1);
+				houseNum.push_back(a2);
+				flatNum.push_back(a3);
+			}
+		}
+		file.close();
+
+	}
+
+	void writeInFile(std::string fileName){
+		std::ofstream file;
+		file.open(fileName);
+		if (file.is_open()) {
+			file << num << "\n";
+			for (int i = std::stoi(num) - 1; i >= 0; i--) {
+				file << city[i] << ", " << street[i] << ", " << houseNum[i] << ", " << flatNum[i] << "\n";
+			}
+		}
+		file.close();
+	}
+
+};
+
+
+int main(){
+
+	Address address("in.txt");
+	address.writeInFile("out.txt");
+
+	return 0;
+}
