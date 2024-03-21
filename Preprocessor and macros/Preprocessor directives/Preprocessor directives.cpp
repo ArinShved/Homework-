02e2c3ca7684049cd50e6f04bef8da14f237dd59
Arina Shvedova <arina4shvedova@gmail.com>
2024-03-21 21:43:18 +0300

    Preprocessor and macros

---
+++ Preprocessor and macros/Preprocessor directives/Preprocessor directives.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+
+#define MODE 1
+
+#if !defined(MODE)
+#error "Ошибка! Определите MODE";
+#endif;
+
+#if MODE == 1
+int add(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	std::cout << "Работаю в боевом режиме. " << "\n" << "Введите первое число: ";
+	int a, b;
+	std::cin >> a;
+	std::cout << "Введите второе число: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: ";
+
+	std::cout << add(a, b);
+	return 0;
+}
+#elif MODE == 0
+int main() {
+	setlocale(LC_ALL, "rus");
+	std::cout << "Работаю в режиме тренировки ";
+	return 0;
+}
+#else 
+int main(){
+	setlocale(LC_ALL, "rus");
+	std::cout << "Неизвестный режим. Завершение работы";
+	return 0;
+}
+#endif; 
+
+
