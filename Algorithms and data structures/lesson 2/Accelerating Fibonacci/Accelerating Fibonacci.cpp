2e4c2c677899eab8e8931b6d989c9d757fa4113b
Arina Shvedova <arina4shvedova@gmail.com>
2024-04-24 16:33:01 +0300

    add lesson 2

---
+++ Algorithms and data structures/lesson 2/Accelerating Fibonacci/Accelerating Fibonacci.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <vector>
+
+
+int fibbonachi(int i, std::vector<int>& memory) {
+    if (memory[i] != -1) {
+        return memory[i];
+    }
+     if (i == 1||i == 0) {
+        return i;
+    }
+     memory[i] = fibbonachi(i - 2, memory) + fibbonachi(i - 1, memory);
+     return memory[i];
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    while (true) {
+        int i;
+        std::cout << "Введите число: ";
+        std::cin >> i;
+        std::vector<int> memory(i + 1, -1);
+
+        std::cout << "Число Фибоначчи: " << fibbonachi(i, memory) << "\n";
+    }
+
+    system("pause");
+    return 0;
+
+}
---
+++ Algorithms and data structures/lesson 2/Fibonacci numbers again/Fibonacci numbers again.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+
+int fibbonachi(int i) {
+    if (i == 0) {
+        return 0;
+    }
+    else if (i == 1) {
+        return 1;
+    }
+    else {
+        return fibbonachi(i - 2) + fibbonachi(i - 1);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    
+    while (true) {
+        int i;
+        std::cout << "Введите число: ";
+        std::cin >> i;
+      
+        std::cout << "Число Фибоначчи: " << fibbonachi(i) << "\n";
+    }
+
+    system("pause");
+    return 0;
+
+}
+
