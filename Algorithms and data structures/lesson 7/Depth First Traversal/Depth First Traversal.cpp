44473576659d4d677bd9d09e575bc6a43f38a198
Arina Shvedova <arina4shvedova@gmail.com>
2024-06-04 17:52:09 +0300

    lesson 7

---
+++ Algorithms and data structures/lesson 7/Depth First Traversal/Depth First Traversal.cpp
@@ -0,0 +1,64 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+void depth_t(std::vector<std::vector<int>> graph, std::vector<int>& visited, int j, int size) {
+	visited[j] = 1;
+	for (int i = 0; i < size; i++) {
+		if (graph[j][i] == 1) {
+			if (visited[i] == 0) {
+				std::cout << " " << i + 1;
+				depth_t(graph, visited, i, size);
+			}
+		}
+	}
+}
+
+
+void depth_f_t(std::vector <std::vector<int>> graph, int size) {
+	std::vector<int> visited(size);
+	visited[0] = 1;
+	for (int i = 1; i < size; i++) {
+		visited[i] = 0;
+	}
+
+	int i = 0;
+	std::cout << "Порядок обхода вершин: " << i + 1 << " ";
+	for (int j = 0; j < size; j++) {
+		if(graph[i][j] == 1) {
+				std::cout << j + 1;
+				depth_t(graph, visited, j, size);
+				break;
+		}
+	}
+}
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int size;
+
+	std::ifstream file("graph.txt");
+	if (file.is_open()) {
+		file >> size;
+		
+		std::vector<std::vector<int>> graph(size + 1, std::vector<int>(size + 1));
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				file >> graph[i][j];
+			}
+		}
+		depth_f_t(graph, size);
+	}
+	else {
+		std::cout << "Файл не найден!";
+		return 0;
+	}
+	file.close();
+
+	return 0;
+}
---
+++ Algorithms and data structures/lesson 7/Wide crawl/Wide crawl.cpp
@@ -0,0 +1,86 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+
+void wide_t(std::vector <std::vector<int>> graph, int size, int num) {
+	std::vector<int> visited(size);
+	for (int i = 0; i < size; i++) {
+		visited[i] = 0;
+	}
+	std::cout << "Порядок обхода вершин: " << " ";
+
+	visited[num-1] = 1;
+	std::queue<int> queue;
+	queue.push(num-1);
+	
+
+	for (int j = 0; j < size; j++) {
+		if (graph[num-1][j] == 1) {
+			if (visited[j] == 0) {
+				queue.push(j);
+				visited[j] = 1;
+			}
+		}
+	}
+
+	while (!queue.empty()) {
+		int i = queue.front();
+		visited[i] = 1;
+		queue.pop();
+		std::cout << i + 1 << " ";
+		for (int j = 0; j < size; j++) {
+			if (visited[j] == 0) {
+				if (graph[i][j] == 1) {
+					queue.push(j);
+					visited[j] = 1;
+				}
+			}
+		}
+	}
+}
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int size;
+	int user_num;
+
+	std::ifstream file("graph.txt");
+	if (file.is_open()) {
+		file >> size;
+
+		std::vector<std::vector<int>> graph(size + 1, std::vector<int>(size + 1));
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				file >> graph[i][j];
+			}
+		}
+		
+
+		std::cout << "В графе " << size <<  " вершин. Введите номер вершины, с которой начнётся обход : ";
+		do {
+			std::cin >> user_num;
+			if (user_num > 0 && user_num <= size) {
+				break;
+			}
+			else {
+				std::cout << "Неверное число! Повторите попытку!\n";
+			}
+		} while (true);
+
+		wide_t(graph, size, user_num);
+	}
+	else {
+		std::cout << "Файл не найден!";
+		return 0;
+	}
+	file.close();
+
+	return 0;
+}
