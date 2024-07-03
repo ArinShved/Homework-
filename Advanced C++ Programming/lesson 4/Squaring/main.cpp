73f11ce218c4678c76a5c7ebd2768192745f4ddb
Arina Shvedova <arina4shvedova@gmail.com>
2024-07-03 13:19:20 +0300

    Remade squaring

--- Advanced C++ Programming/lesson 4/Squaring/main.cpp
+++ Advanced C++ Programming/lesson 4/Squaring/main.cpp
@@ -6,8 +6,8 @@ T squaring(T a) {
 	return a * a;
 }
 
-template <>
-std::vector<int> squaring(std::vector<int> arr) {
+template <class T>
+std::vector<T> squaring(std::vector<T> arr) {
 	std::vector<int> temp(arr.size());
 	for (int i = 0; i < arr.size(); i++) {
 		temp[i] = arr[i] * arr[i];
@@ -15,7 +15,8 @@ std::vector<int> squaring(std::vector<int> arr) {
 	return temp;
 }
 
-void print_arr(std::vector<int> arr) {
+template <class T>
+void print_arr(std::vector<T> arr) {
 	for (int i = 0; i < arr.size(); i++) {
 		std::cout << arr[i] << " ";
 	}
