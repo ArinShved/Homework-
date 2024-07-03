4a98068e4b85e0f41d8f07aef3edfd12843ab4cf
Arina Shvedova <arina4shvedova@gmail.com>
2024-07-03 15:55:50 +0300

    Add lesson 6

---
+++ Advanced C++ Programming/lesson 6/Unique vector/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+void sort_arr(std::vector<int>& arr) {
+	std::sort(arr.begin(), arr.end());
+	auto it = std::unique(arr.begin(), arr.end());
+	arr.erase(it, arr.end());
+}
+
+int main() {
+	std::vector<int> arr = { 1, 1, 2, 5, 6, 1, 2, 4 };
+	std::cout << "[IN]:";
+	for (auto& i : arr) {
+		std::cout << i << " ";
+	}
+
+	sort_arr(arr);
+
+	std::cout << "\n[OUT]: ";
+	for (auto& i : arr) {
+		std::cout << i << " ";
+	}
+
+	return 0;
+}
