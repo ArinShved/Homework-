c0bc97438e98ebc6b55f0226b5aa9754f8c57a73
Arina Shvedova <arina4shvedova@gmail.com>
2024-09-27 14:47:16 +0300

    remade requests

--- Working with databases/lesson4/main.cpp
+++ Working with databases/lesson4/main.cpp
@@ -24,43 +24,59 @@ public:
     }
     
     void add_client(const Client& client, pqxx::connection& c) {
-        pqxx::work w(c);
-        w.exec("INSERT INTO clients(name, surname, email) VALUES('" + client.name + "','"
-            + client.surname + "', '" + client.email + "')");
-        w.commit();
+        if (check_words(client.name) && check_words(client.surname)) {
+            pqxx::work w(c);
+            w.exec_params("INSERT INTO clients(name, surname, email) VALUES($1, $2, $3)", client.name,
+                client.surname, client.email);
+            w.commit();
+        }
+        else {
+            std::cout << "Введенные данные содержат недопустимые слова. Пожалуйста, повторите попытку с другим Именем или Фамилией\n";
+        }
     }
 
-    void add_phone(const Client& client, const std::string& phone, pqxx::connection& c) {
-        pqxx::work w(c);
-        w.exec("INSERT INTO phones (client_id, phone) VALUES ((SELECT id FROM clients WHERE email = '"
-            + client.email + "'),'" + phone + "')");
-        w.commit();
+    void add_phone(const std::string& email, const std::string& phone, pqxx::connection& c) {
+        if (check_words(phone)) {
+            pqxx::work w(c);
+            w.exec_params("INSERT INTO phones (client_id, phone) VALUES ((SELECT id FROM clients WHERE email = $1), $2)",
+                email, phone);
+                w.commit();
+        }
+        else {
+            std::cout << "Неверный ввод номера\n";
+        }
     }
 
     void update_client(const Client& client, int id, pqxx::connection& c) {
+        if (check_words(client.name) && check_words(client.surname)) {
+            pqxx::work w(c);
+            w.exec_params("UPDATE clients SET name = $1, surname = $2, email = $3  WHERE id = $4",
+                client.name, client.surname, client.email, std::to_string(id));
+            w.commit();
+        }
+        else {
+            std::cout << "Введенные данные содержат недопустимые слова. Пожалуйста, повторите попытку с другим Именем или Фамилией\n";
+        }
+
+    }
+
+    void remove_phone(const std::string& email, const std::string& phone, pqxx::connection& c) {
         pqxx::work w(c);
-        w.exec("UPDATE clients SET name = '" +client.name +"', surname = '" + client.surname + 
-            "', email = '" + client.email + "' WHERE id = " + std::to_string(id));
+        w.exec_params("DELETE FROM phones WHERE client_id = (SELECT id FROM clients WHERE email = $1) AND phone = $2",
+            email, phone);
         w.commit();
     }
 
-    void remove_phone(const Client& client, const std::string& phone, pqxx::connection& c) {
+    void remove_client(const std::string& email, pqxx::connection& c) {
         pqxx::work w(c);
-        w.exec("DELETE FROM phones WHERE client_id = (SELECT id FROM clients WHERE email = '"
-            + client.email +"') AND phone = '"+ phone + "'");
-        w.commit();
-    }
-
-    void remove_client(const Client& client, pqxx::connection& c) {
-        pqxx::work w(c);
-        w.exec("DELETE FROM phones WHERE client_id = (SELECT id FROM clients WHERE email = '"+ client.email +"')");
-        w.exec("DELETE FROM clients WHERE email = '" + client.email +"'");
+        w.exec_params("DELETE FROM phones WHERE client_id = (SELECT id FROM clients WHERE email = $1)", email);
+        w.exec_params("DELETE FROM clients WHERE email = $1", email );
         w.commit();
     }
     
     Client find_by_Email(const std::string& email, pqxx::connection& c) {
         pqxx::work w(c);
-        pqxx::result r = w.exec("SELECT id, name, surname, email FROM clients WHERE email = '" + email + "'");
+        pqxx::result r = w.exec_params("SELECT id, name, surname, email FROM clients WHERE email = $1", email);
         if (r.size() == 1) {
             Client client;
             client.id = r[0]["id"].as<int>();
@@ -78,7 +94,7 @@ public:
 
     Client find_by_name(const std::string& name, pqxx::connection& c) {
         pqxx::work w(c);
-        pqxx::result r = w.exec("SELECT id, name, surname, email FROM clients WHERE name LIKE '%' ||'" + name + "' || '%'");
+        pqxx::result r = w.exec_params("SELECT id, name, surname, email FROM clients WHERE name LIKE '%' || $1 || '%'", name);
         if (r.size() == 1) {
             Client client;
             client.id = r[0]["id"].as<int>();
@@ -96,7 +112,7 @@ public:
 
     Client find_by_surname(const std::string& surname, pqxx::connection& c) {
         pqxx::work w(c);
-        pqxx::result r = w.exec("SELECT id, name, surname, email FROM clients WHERE surname LIKE '%' ||'" + surname +"' || '%'");
+        pqxx::result r = w.exec_params("SELECT id, name, surname, email FROM clients WHERE surname LIKE '%' || $1 || '%'", surname);
         if (r.size() == 1) {
             Client client;
             client.id = r[0]["id"].as<int>();
@@ -112,8 +128,25 @@ public:
         }
     }
 
-    
+    friend bool check_words(std::string str);
+
+   
 };
+
+bool check_words(std::string str) {
+    std::string check = str;
+    for (int i = 0; i < str.length(); i++) {
+        check[i] = std::toupper(str[i]);
+    }
+    std::vector<std::string> ban_words = { "SELECT","INSERT", "UPDATE","DELETE","DROP", "TRUNCATE","CREATE","ALTER",
+        "GRANT","REVOKE","COMMIT","ROLLBACK", "INFORMATION_SCHEMA", "SYS.TABLES", "SYS.COLUMNS"};
+    for (auto i : ban_words) {
+        if (check.find(i) != std::string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 int main() {
@@ -129,24 +162,24 @@ int main() {
             "password=9757y_Q1");
 
         Database base;
-      
-        Client us{ "Luke", "Holle", "oit.com" };
-        Client user_1{ "Andru", "Lock", "uu.com" };
+
+        Client us{"Luke", "Holle", "orer.com"};
+        Client user_1{ "Andru", "Lock", "uuu.com" };
         base.create_tables(c);
         base.add_client(us, c);
-        base.add_client(user_1, c);
-        base.add_phone(us, "8(966)987-09-09", c);
-        base.add_phone(us, "8(767)987-09-98", c);
+        //base.add_client(user_1, c);
+        base.add_phone("orer.com", "8(966)987-09-09", c);
+        base.add_phone("orer.com", "8(767)987-09-98", c);
 
-        Client user_2{ "Luke", "Ghost", "ot.com" };
-        base.update_client(user_2, 1, c);
-        base.remove_phone(us, "8(767)987-09-98", c);
+        Client user_2{ "Luke", "Ghost", "otqq.com" };
+        //base.update_client(user_2, 1, c);
+        base.remove_phone("orer.com", "8(767)987-09-98", c);
         
-        base.find_by_Email("oit.com", c);
+        base.find_by_Email("otqq.com", c);
         base.find_by_name("Andru", c);
         base.find_by_surname("Ghost", c);
 
-        base.remove_client(us, c);
+      //  base.remove_client("orer.com", c);
     }
     catch (pqxx::sql_error e) {
         std::cout << e.what() << "\n";
