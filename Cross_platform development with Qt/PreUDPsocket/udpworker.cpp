5ff4c19f85d10bacfff70760d3a4296b754f17c9
Arina Shvedova <arina4shvedova@gmail.com>
2025-04-07 23:47:32 +0300

    Add UDP

---
+++ Cross_platform development with Qt/PreUDPsocket/udpworker.cpp
@@ -0,0 +1,87 @@
+#include "udpworker.h"
+
+UDPworker::UDPworker(QObject *parent) : QObject(parent)
+{
+
+
+
+}
+
+
+/*!
+ * @brief Метод инициализирует UDP сервер
+ */
+void UDPworker::InitSocket()
+{
+
+    serviceUdpSocket = new QUdpSocket(this);
+    /*
+     * Соединяем присваиваем адрес и порт серверу и соединяем функцию
+     * обраотчик принятых пакетов с сокетом
+     */
+    serviceUdpSocket->bind(QHostAddress::LocalHost, BIND_PORT);
+
+    connect(serviceUdpSocket, &QUdpSocket::readyRead, this, &UDPworker::readPendingDatagrams);
+
+}
+
+/*!
+ * @brief Метод осуществляет обработку принятой датаграммы
+ */
+void UDPworker::ReadDatagram(QNetworkDatagram datagram)
+{
+
+    QByteArray data;
+    data = datagram.data();
+
+
+
+    QDataStream inStr(&data, QIODevice::ReadOnly);
+    QDateTime dateTime;
+    inStr >> dateTime;
+
+    if(dateTime.isValid()&& inStr.status() == QDataStream::Ok){
+
+        emit sig_sendTimeToGUI(dateTime);
+
+    }
+    else{
+
+         QString text_data = QString::fromUtf8(data);
+         emit sig_sendTextToGUI(text_data, datagram.senderPort(), data.size());
+    }
+
+
+}
+/*!
+ * @brief Метод осуществляет опередачу датаграммы
+ */
+void UDPworker::SendDatagram(QByteArray data)
+{
+    /*
+     *  Отправляем данные на localhost и задефайненный порт
+     */
+    serviceUdpSocket->writeDatagram(data, QHostAddress::LocalHost, BIND_PORT);
+}
+
+void UDPworker::SendTextDatagram(QString data)
+{
+    QByteArray text_data = data.toUtf8();
+    serviceUdpSocket->writeDatagram(text_data, QHostAddress::LocalHost, BIND_PORT);
+}
+
+
+/*!
+ * @brief Метод осуществляет чтение датаграм из сокета
+ */
+void UDPworker::readPendingDatagrams( void )
+{
+    /*
+     *  Производим чтение принятых датаграмм
+     */
+    while(serviceUdpSocket->hasPendingDatagrams()){
+            QNetworkDatagram datagram = serviceUdpSocket->receiveDatagram();
+            ReadDatagram(datagram);
+    }
+
+}
